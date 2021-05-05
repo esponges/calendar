1f10c760e331feb37ee114c05bcc0c60b0cfcfc1
esponges <esponges@gmail.com>
2021-05-05 10:35:53 -0500

    get real time month names

--- src/tests/Calendar.test.js
+++ src/tests/Calendar.test.js
@@ -1,6 +1,7 @@
 import { cleanup, getByRole, render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Calendar from "../components/Calendar";
+import months from '../services/months';
 
 beforeEach(() => {
   render(
@@ -10,32 +11,45 @@ beforeEach(() => {
 
 afterEach(cleanup);
 
+const actualMonthValue = new Date().getMonth();
+const getMonthName = (offset = 0) => {
+  const monthName = months[actualMonthValue + offset].month;
+    return monthName;
+  };
+const monthName = getMonthName();
+const actualYear = new Date().getFullYear();
+
+
 describe("Calendar", () => {
   it("renders", () => {
     render(<Calendar />);
   });
 
   it("displays actual month and year", () => {
-    const heading = screen.getByRole("heading", { name: /april 2021/i });
+    const heading = screen.getByRole("heading", { name: `${monthName} ${actualYear}` });
     expect(heading).toBeInTheDocument();
   });
 
   it('displays last month when clicking "back"', () => {
+    const lastMonthName = getMonthName(-1);
+
     const backBtn = screen.getByRole("button", { name: /back/i });
     userEvent.click(backBtn);
 
     waitFor(() => {
-      const heading = screen.getAllByRole("heading", { name: /march 2021/i });
+      const heading = screen.getAllByRole("heading", { name: `${lastMonthName} ${year}` });
       expect(heading).toBeInTheDocument();
     });
   });
 
   it('displays last month when clicking "next"', () => {
+    const nextMonthName = getMonthName(1);
+
     const nextBtn = screen.getByRole("button", { name: /next/i });
     userEvent.click(nextBtn);
 
     waitFor(() => {
-      const heading = screen.getAllByRole("heading", { name: /may 2021/i });
+      const heading = screen.getAllByRole("heading", { name: `${nextMonthName} ${year}` });
       expect(heading).toBeInTheDocument();
     });
   });
